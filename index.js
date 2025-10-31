39ddc9d7b5efbe72ef156bd4402c228609b67676
cavid <cavokiller.az>
2025-11-01 00:20:03 +0400

    Optimized 2.0

--- index.js
+++ index.js
@@ -1,149 +1,112 @@
 // Buttons
 const metric = document.getElementById("metric-btn")
 const imperial = document.getElementById("imperial-btn")
-
-// Form
-const form = document.getElementById("bmi-form")
-const resultDisplay = document.getElementById("result-display")
-
-// Inputs
-const metricInputs = document.getElementById("metric-inputs")
-const imperialInput = document.getElementById("imperial-inputs")
-
-// Error Message
-let errorMesage = document.getElementById("error-message")
-
-// Toggle Display Functions
-let showMetric = () => {
-    metricInputs.style.display = 'block';
-    imperialInput.style.display = 'none';
-    metric.classList.add('active');
-    imperial.classList.remove('active');
-}
-
-let showImperial = () => {
-    imperialInput.style.display = 'block';
-    metricInputs.style.display = 'none';
-    imperial.classList.add('active');
-    metric.classList.remove('active');
-}
-
-// Call Functions On Button Click
-metric.addEventListener('click', showMetric);
-imperial.addEventListener('click', showImperial);
-
-// Calculate And Clear Button
 const calculate = document.getElementById("calculate-btn")
 const clear = document.getElementById("clear-btn")
 
-// Calculate BMI
+// ResultDiv Elements
+const resultDisplay = document.getElementById("result-display")
 let bmiValueElement = document.getElementById("bmi-value");
 let bmiCategoryElement = document.getElementById("bmi-category");
 let image = document.getElementById("image")
 
+// Form Element
+const form = document.getElementById("bmi-form") // 
+
+// Inputs
+const metricInputs = document.getElementById("metric-inputs")
+const imperialInputs = document.getElementById("imperial-inputs")
+let weight = document.getElementById("metric-weight");
+let height = document.getElementById("metric-height");
+let feet = document.getElementById("imperial-height-ft");
+let inches = document.getElementById("imperial-height-in");
+let pounds = document.getElementById("imperial-weight");
+
+// Error Message
+let errorMesage = document.getElementById("error-message") 
+
+// Functions
+metric.addEventListener("click", () => {
+    metricInputs.style.display = 'block';
+    imperialInputs.style.display = 'none';
+    metric.classList.add('active');
+    imperial.classList.remove('active');
+});
+
+imperial.addEventListener("click" , () => {
+    imperialInputs.style.display = 'block';
+    metricInputs.style.display = 'none';
+    imperial.classList.add('active');
+    metric.classList.remove('active');
+});
+
+let updateResults = (bmi) => {
+    if (bmi < 18.5){
+        bmiCategoryElement.innerText = "Underweight";
+        image.src = "UnderWeight.png"
+    }
+    else if(bmi < 25){
+        bmiCategoryElement.innerText = "Normal";
+        image.src = "NormalWeight.png"
+    }
+    else if(bmi < 30){
+        bmiCategoryElement.innerText = "Overweight";
+        image.src = "overweighted.png"
+    }
+    else{
+        bmiCategoryElement.innerText = "Obesity";
+        image.src = "Obesity.png"
+    }
+
+    resultDisplay.style.display = "block";
+    errorMesage.style.display = "none";
+}
+
+let checkInputs = (input) => {
+    inputVal = Number(input.value)
+    if(inputVal <= 0 || isNaN(inputVal)){
+        input.style.borderColor = "red";
+        errorMesage.style.display = "block";
+        return false
+    }
+    else{
+        input.style.borderColor = "#D1D5DB";
+        return true
+    };
+}
 calculate.addEventListener("click", () => {
     if (metric.classList.contains("active")) {
-        let weight = document.getElementById("metric-weight");
-        let height = document.getElementById("metric-height");
-        if (weight.value == false || weight.value <= "0"){
-            weight.style.borderColor = "red";
-            errorMesage.style.display = "block";
-        }
-        else{
-            weight.style.borderColor = "gray";
-        }
-        if (height.value == false || height.value <= "0"){
-            height.style.borderColor = "red";
-            errorMesage.style.display = "block";
-        }
-        else{
-            height.style.borderColor = "gray";
-        }
-        // if everything ok
-        if (weight.value > 0 && height.value > 0){
-            errorMesage.style.display = "none";
+        checkInputs(weight);checkInputs(height);
+        if (checkInputs(weight) && checkInputs(height)){
             let bmi = (Number(weight.value) / ((Number(height.value)/100) ** 2)).toFixed(2); // calculates bmi
             bmiValueElement.innerText = `${bmi}`; 
-            // checks categories
-            if (bmi < 18.5){
-                bmiCategoryElement.innerText = "Underweight"; 
-                image.src = "UnderWeight.png"
-            }
-            else if(bmi < 25){
-                bmiCategoryElement.innerText = "Normal"; 
-                image.src = "NormalWeight.png"
-            }
-            else if(bmi < 30){
-                bmiCategoryElement.innerText = "Overweight"; 
-                image.src = "overweighted.png"
-            }
-            else{
-                bmiCategoryElement.innerText = "Obesity";
-                image.src = "Obesity.png"
-            }
-            resultDisplay.style.display = "block";
+            updateResults(bmi)
         }
     }
     else {
-        let feet = document.getElementById("imperial-height-ft");
-        let inches = document.getElementById("imperial-height-in");
-        let pounds = document.getElementById("imperial-weight");
-
-        if (pounds.value == false || pounds.value <= "0"){
-            pounds.style.borderColor = "red";
-            errorMesage.style.display = "block";
-        }
-        else{
-            pounds.style.borderColor = "gray";
-        }
-        if (feet.value == false || inches.value == false || (feet.value <= "0" && inches.value <= "0")){
-            feet.style.borderColor = "red";
-            inches.style.borderColor = "red";
-            errorMesage.style.display = "block";
-        }
-        else{
-            feet.style.borderColor = "gray";
-            inches.style.borderColor = "gray";
-        }
-        // if everything ok
-        if (pounds.value > 0 && (Number(feet.value) > 0 || Number(inches.value) > 0)){
-            errorMesage.style.display = "none";
+        checkInputs(pounds);checkInputs(feet);checkInputs(inches);
+        if (checkInputs(pounds) && checkInputs(feet) && checkInputs(inches)){  
             let totalInches = (Number(feet.value) * 12) + Number(inches.value);
             let bmi = ((Number(pounds.value) * 703) / (totalInches ** 2)).toFixed(2);
             bmiValueElement.innerText = `${bmi}`; 
-            
-            if (bmi < 18.5){
-            bmiCategoryElement.innerText = "Underweight";
-            image.src = "UnderWeight.png"
-            }
-            else if(bmi < 25){
-            bmiCategoryElement.innerText = "Normal";
-            image.src = "NormalWeight.png"
-            }
-            else if(bmi < 30){
-            bmiCategoryElement.innerText = "Overweight";
-            image.src = "overweighted.png"
-            }
-            else{
-            bmiCategoryElement.innerText = "Obesity";
-            image.src = "Obesity.png"
-            }
-            resultDisplay.style.display = "block";
+            updateResults(bmi)
         }
     }
 });
 
 clear.addEventListener("click", () => {
-    document.getElementById("metric-weight").value = "";
-    document.getElementById("metric-height").value = "";
-    document.getElementById("imperial-height-ft").value = "";
-    document.getElementById("imperial-height-in").value = "";
-    document.getElementById("imperial-weight").value = "";
-    document.getElementById("metric-weight").style.borderColor = "gray";
-    document.getElementById("metric-height").style.borderColor = "gray";
-    document.getElementById("imperial-height-ft").style.borderColor = "gray";
-    document.getElementById("imperial-height-in").style.borderColor = "gray";
-    document.getElementById("imperial-weight").style.borderColor = "gray";
+    weight.value = "";
+    height.value = "";
+    feet.value = "";
+    inches.value = "";
+    pounds.value = "";
+    pounds.value,borderColor= "","#D1D5DB";
+    weight.style.borderColor = "#D1D5DB";
+    height.style.borderColor = "#D1D5DB";
+    feet.style.borderColor = "#D1D5DB";
+    inches.style.borderColor = "#D1D5DB";
+    pounds.style.borderColor = "#D1D5DB";
     errorMesage.style.display = "none";
     resultDisplay.style.display = "none";
+    image.src = "";
 });
